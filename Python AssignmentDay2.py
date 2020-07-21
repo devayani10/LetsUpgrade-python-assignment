36ac138ac69a8584ebbcc79b730d4d5b3031a272
devayani10 <Devayani10022000@gmail.com>
2020-07-21 17:48:56 +0530

    Add files via upload

---
+++ Python AssignmentDay2.py
@@ -0,0 +1,477 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello")
+
+
+# In[2]:
+
+
+print("hello
+      python")
+
+
+# In[3]:
+
+
+print("hellopython")
+
+
+# In[4]:
+
+
+print(""" /\_/   ____/ o o  /~____  =ø= /
+(______)__m_m)""")
+
+
+# In[5]:
+
+
+print('hello python')
+
+
+# In[6]:
+
+
+print('python's world')
+
+
+# In[7]:
+
+
+print("python's world")
+
+
+# In[8]:
+
+
+print('python\'s world')
+
+
+# In[9]:
+
+
+print("hello\tworld")
+
+
+# In[10]:
+
+
+print("hello\npython")
+
+
+# In[11]:
+
+
+name="abde"
+age=20
+marks=90
+print("the name of person is","abde")
+
+
+# In[12]:
+
+
+print("the name of person is",name,"marks is",marks,"age is",age)
+
+
+# In[13]:
+
+
+name="abde"
+age=20
+marks=90.876
+print("the name of person is %s marks is %f age is %d(name,marks,age)")
+
+
+# In[14]:
+
+
+print("the name of person is %10s marks is %10f age is %10d(name,marks,age)")
+
+
+# In[15]:
+
+
+print("the name of person is %s marks is %10.2f age is %d(name,marks,age)")
+
+
+# In[16]:
+
+
+print(f"the name of person is{name}the marls is{marks}age is{age}")
+
+
+# In[17]:
+
+
+x=y=10
+
+
+# In[18]:
+
+
+x
+
+
+# In[19]:
+
+
+y
+
+
+# In[20]:
+
+
+id(x)
+
+
+# In[21]:
+
+
+id(y)
+
+
+# In[22]:
+
+
+del x
+
+
+# In[23]:
+
+
+y
+
+
+# In[24]:
+
+
+del y
+
+
+# In[25]:
+
+
+x=y=20
+
+
+# In[26]:
+
+
+5**5
+
+
+# In[27]:
+
+
+10+20
+
+
+# In[28]:
+
+
+10-20
+
+
+# In[29]:
+
+
+10/2
+
+
+# In[30]:
+
+
+10/3
+
+
+# In[31]:
+
+
+10%3
+
+
+# In[32]:
+
+
+10//3
+
+
+# In[33]:
+
+
+a=10
+b=20
+a==b
+
+
+# In[34]:
+
+
+c=10
+a==c
+
+
+# In[35]:
+
+
+c<a
+
+
+# In[36]:
+
+
+c<=a
+
+
+# In[37]:
+
+
+a>b
+
+
+# In[38]:
+
+
+a>=b
+
+
+# In[39]:
+
+
+a!=b
+
+
+# In[40]:
+
+
+a+=b
+
+
+# In[41]:
+
+
+a=10
+a|b
+
+
+# In[42]:
+
+
+a&b
+
+
+# In[43]:
+
+
+a~b
+
+
+# In[44]:
+
+
+a^b
+
+
+# In[45]:
+
+
+a<<b
+
+
+# In[46]:
+
+
+a>>b
+
+
+# In[47]:
+
+
+a and b
+
+
+# In[48]:
+
+
+a or b
+
+
+# In[49]:
+
+
+a not b
+
+
+# In[50]:
+
+
+a>20 or 10<9
+
+
+# In[51]:
+
+
+a>20 and 10>9
+
+
+# In[52]:
+
+
+10>9 and 20>m
+
+
+# In[53]:
+
+
+10<9 and 20>m
+
+
+# In[54]:
+
+
+10<9
+
+
+# In[55]:
+
+
+not 10<9
+
+
+# In[56]:
+
+
+not true
+
+
+# In[57]:
+
+
+str1 ="pythonprogramming" "a" in str1
+
+
+# In[58]:
+
+
+str1="pythonprogramming" "x" not in str1
+
+
+# In[59]:
+
+
+a=10
+b=10
+a==b
+
+
+# In[60]:
+
+
+a is b
+
+
+# In[61]:
+
+
+id(a)
+
+
+# In[62]:
+
+
+id(b)
+
+
+# In[64]:
+
+
+x=1.5
+y=1.5
+x==y
+
+
+# In[65]:
+
+
+x is y
+
+
+# In[66]:
+
+
+id(x)
+
+
+# In[67]:
+
+
+id(y)
+
+
+# In[68]:
+
+
+a=257
+b=257
+a==b
+
+
+# In[69]:
+
+
+a is b
+
+
+# In[70]:
+
+
+10+10 / 29*4
+
+
+# In[71]:
+
+
+10/29
+
+
+# In[72]:
+
+
+2**-1
+
+
+# In[73]:
+
+
+a=100
+b=100
+
+
+# In[74]:
+
+
+7/3
+
+
+# In[75]:
+
+
+7//3
+
+
+# In[76]:
+
+
+10>8 and 8<9
+
+
+# In[ ]:
+
+
+
+
